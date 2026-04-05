349daf40f89912f9c193e995ed21fcd21ced3f33
Kevin <kevinarmenteros@icloud.com>
2026-04-05 12:47:11 -0400

    tidy up capitalization and add AUTO_INCREMENT so DB handles ID generation vs manual app code

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,29 +2,29 @@ CREATE DATABASE IF NOT EXISTS food_reviews;
 USE food_reviews;
 
 CREATE TABLE  User(
-    userID INT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     PasswordHash VARCHAR(255) NOT NULL
     
 );
 
 
 CREATE TABLE  FoodSpot(
-    spotID int PRIMARY KEY,
-    picture varchar(255), /* stores the file name dont want to store iamges in database isntead speerate folder to then grab when needed isntead */
-    address varchar(255),
-    name varchar(255)
+    spotID INT PRIMARY KEY AUTO_INCREMENT,
+    picture VARCHAR(255), /* stores the file name dont want to store iamges in database isntead speerate folder to then grab when needed isntead */
+    address VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE  Review(
-    reviewID int PRIMARY KEY,
-    rating int, /* assuming users can only rate it 1,2,3,4, or 5 stars only*/
-    comment varchar(1000), 
+    reviewID INT PRIMARY KEY AUTO_INCREMENT,
+    rating INT NOT NULL, 
+    comment VARCHAR(1000), 
     reviewDate DATETIME,
-    userID int,
-    spotID int,
+    userID INT,
+    spotID INT,
     FOREIGN KEY (spotID) REFERENCES FoodSpot(spotID),
     FOREIGN KEY (userID) REFERENCES User(userID)
 
